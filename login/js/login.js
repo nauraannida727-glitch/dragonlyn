9919bbcb83d4af074104a357f40851099049da77
nauraannida727-glitch <nauraannida727@gmail.com>
2026-05-09 07:20:12 +0000

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,36 +1,18 @@
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+    const form = document.querySelector("form");
 
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+    form.addEventListener("submit", function(e) {
+
+        e.preventDefault();
+
+        const email =
+            document.querySelector('input[type="text"]').value;
+
+        localStorage.setItem("username", email);
+
+        window.location.href = "../index.html";
+
     });
 
-    const data = await res.json();
-
-    if (data.status === "success") {
-        // simpan username
-            localStorage.setItem("username", data.username);
-            window.location.href = "../index.html";
-         
-    // } else {
-    //     document.getElementById("message").innerText = "Username / Password salah";alert("Login gagal");
-    // }
-    
-    } else {
-    const alertBox = document.getElementById("alertBox");
-    alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
-    alertBox.style.display = "block";
-
-    setTimeout(() => {
-        alertBox.style.display = "none";
-    }, 3000);
-} 
-   
 });
