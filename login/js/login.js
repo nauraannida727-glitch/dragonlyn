2e7c35ab842547c56c8330b8ba22b70800804bda
nauraannida727-glitch <nauraannida727@gmail.com>
2026-05-14 09:59:14 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,36 +1,38 @@
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
+document.getElementById("loginForm").addEventListener("submit", function(e){
+
     e.preventDefault();
 
     const username = document.getElementById("username").value.trim();
+
     const password = document.getElementById("password").value.trim();
 
-    const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-    });
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
-    const data = await res.json();
+    const validUser = users.find(user =>
+        user.username === username &&
+        user.password === password
+    );
 
-    if (data.status === "success") {
-        // simpan username
-            localStorage.setItem("username", data.username);
-            window.location.href = "../index.html";
-         
-    // } else {
-    //     document.getElementById("message").innerText = "Username / Password salah";alert("Login gagal");
-    // }
-    
+    if(validUser){
+
+        localStorage.setItem("username", validUser.username);
+
+        alert("Login berhasil!");
+
+        window.location.href = "../index.html";
+
     } else {
-    const alertBox = document.getElementById("alertBox");
-    alertBox.innerText = "Username atau Password salah, silahkan coba lagi";
-    alertBox.style.display = "block";
 
-    setTimeout(() => {
-        alertBox.style.display = "none";
-    }, 3000);
-} 
-   
+        const alertBox = document.getElementById("alertBox");
+
+        alertBox.innerText =
+            "Username atau Password salah";
+
+        alertBox.style.display = "block";
+
+        setTimeout(() => {
+            alertBox.style.display = "none";
+        }, 3000);
+    }
+
 });
